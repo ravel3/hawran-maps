c362b2fc9f9d772bd8575f82b60fbe68cdb16cdf
ravel <john.doe@example.com>
2025-06-18 00:25:25 +0200

    tabel + poprawa mapy

--- medale.js
+++ medale.js
@@ -1,31 +1,36 @@
 // Konfiguracja medalów dla Beskidu Niskiego
+
 // Współrzędne X,Y odnoszą się do pozycji na obrazie mapy (1536x1085 px)
+
 // X - pozycja pozioma od lewej krawędzi (0-1536)
+
 // Y - pozycja pionowa od górnej krawędzi (0-1085)
-//  y=1809-
+
+// y=1809-
+
 const medale = [
     {
         id: 1,
         nazwa: "Kopanka",
         opis: "Kopanka",
-        x: 190,
-        y: 1531,
+        x: 114,
+        y: 918,
         poczatkowa_ilosc: 5
     },
     {
         id: 2,
-        nazwa: "Lipa", 
+        nazwa: "Lipa",
         opis: "Lipa opis",
-        x: 259,
-        y: 1313,
+        x: 155,
+        y: 787,
         poczatkowa_ilosc: 3
     },
     {
         id: 3,
         nazwa: "Złamane drzewo",
         opis: "Złamane drzewo - opis",
-        x: 620,
-        y: 1603,
+        x: 372,
+        y: 960,
         poczatkowa_ilosc: 4
     }
 ];
